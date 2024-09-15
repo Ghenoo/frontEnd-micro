1af270ad87a863b3c0c2c9ae30efd3a8ed04191f
Ghenoo <lucasgheno600@gmail.com>
2024-09-14 23:56:13 -0300

    fix: add process.env for connect if backend and vercel

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'; // Use a URL da variável de ambiente ou uma URL padrão para desenvolvimento
 
 interface Item {
     id: number;
